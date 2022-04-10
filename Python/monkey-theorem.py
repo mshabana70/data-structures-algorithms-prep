ec371c7348384a1a9e9e55382643fce21f737fad
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-09 21:06:42 -0400

    start of monkey theorem problem

---
+++ Python/monkey-theorem.py
@@ -0,0 +1,19 @@
+# Monkey Theorem Self Check
+# https://runestone.academy/ns/books/published/pythonds3/Introduction/DefiningFunctions.html
+
+# import random module
+import random
+
+# create a list of possible values for our string
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
+"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+target_string = "methinks it is like a weasel"
+# Write the first function to generate a random string 28 characters long
+def generate_random():
+    random_string = ""
+    for _ in range(len(target_string)):
+        random_string += random.choice(letters)
+    return random_string
+
+generated_string = generate_random()
+print(generated_string)
