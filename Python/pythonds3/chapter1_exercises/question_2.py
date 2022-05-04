d0914674f692211cb82af24ae2ecbd94fee3c41e
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-04 14:50:58 -0400

    Question 2 of chapter 1

---
+++ Python/pythonds3/chapter1_exercises/question_2.py
@@ -0,0 +1,30 @@
+"""
+2. In many ways it would be better if all fractions were maintained in lowest terms right from the start. Modify the constructor for the Fraction class so that GCD is used to reduce fractions immediately. Notice that this means the __add__ function no longer needs to reduce. Make the necessary modifications.
+"""
+
+class Fraction:
+    """Class Fraction"""  
+    def __init__(self, top, bottom):
+        """Constructor definition"""
+        self.common = self.gcd(top, bottom)
+        self.num = top // self.common
+        self.den = bottom // self.common
+    
+    def get_num(self):
+        """Method that returns the numerator of a fraction."""
+        return {self.num}
+    
+    def get_den(self):
+        """Method that returns the denominator of a fraction."""
+        return {self.den}
+    
+    def gcd(self, m, n):
+        while m % n != 0:
+            m, n = n, m % n
+        return n
+
+    def show(self):
+        print(f"{self.num}/{self.den}")
+
+    def __str__(self):
+        return f"{self.num}/{self.den}"
