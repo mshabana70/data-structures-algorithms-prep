ecb815b54100bfa57db30a8ab56f4caad3edd338
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-04 14:35:35 -0400

    show and str methods for displaying fraction object

--- Python/pythonds3/chapter1_exercises/question_1.py
+++ Python/pythonds3/chapter1_exercises/question_1.py
@@ -9,3 +9,9 @@ class Fraction:
         """Constructor definition"""
         self.num = top
         self.den = bottom
+
+    def show(self):
+        print(f"{self.num}/{self.den}")
+
+    def __str__(self):
+        return f"{self.num}/{self.den}"
