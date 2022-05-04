291dbb87c0e1e0474b751ca8e66c2bcd6f86344c
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-04 14:40:32 -0400

    included gcd to constructor (simplfies fraction initially

--- Python/pythonds3/chapter1_exercises/question_1.py
+++ Python/pythonds3/chapter1_exercises/question_1.py
@@ -4,14 +4,24 @@ return the numerator and denominator of a fraction.
 """
 
 class Fraction:
-    """Class Fraction"""
+    """Class Fraction"""  
     def __init__(self, top, bottom):
         """Constructor definition"""
-        self.num = top
-        self.den = bottom
+        self.common = self.gcd(top, bottom)
+        self.num = top // self.common
+        self.den = bottom // self.common
+    
+    def gcd(self, m, n):
+        while m % n != 0:
+            m, n = n, m % n
+        return n
 
     def show(self):
         print(f"{self.num}/{self.den}")
 
     def __str__(self):
         return f"{self.num}/{self.den}"
+
+
+my_fraction = Fraction(6, 8)
+print(my_fraction)
