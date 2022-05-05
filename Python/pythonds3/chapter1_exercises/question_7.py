935abef1e8c3d9b67899e7db4b961b81dc132a33
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-05 15:02:14 -0400

    start of question 7

--- Python/pythonds3/chapter1_exercises/question_7.py
+++ Python/pythonds3/chapter1_exercises/question_7.py
@@ -2,3 +2,30 @@
 7. Research the __radd__ method. How does it differ from __add__? When is it used? Implement __radd__.
 """
 
+class Fraction:
+    """Class Fraction"""  
+    def __init__(self, top, bottom):
+        """Constructor definition"""
+        if type(top) is int and type(bottom) is int:
+            if bottom < 0:
+                self.common = self.gcd((top * -1), (bottom * -1))
+                self.num = (top // self.common) * -1
+                self.den = (bottom * -1) // self.common
+            else:
+                self.common = self.gcd(top, bottom)
+                self.num = top // self.common
+                self.den = bottom // self.common
+        else:
+            raise RuntimeError("ERROR: Invalid entry for fraction. Please enter integer values for the numerator and denominator")
+
+    def gcd(self, m, n):
+        while m % n != 0:
+            m, n = n, m % n
+        return n
+
+    def show(self):
+        print(f"{self.num}/{self.den}")
+
+    def __str__(self):
+        return f"{self.num}/{self.den}"
+        
